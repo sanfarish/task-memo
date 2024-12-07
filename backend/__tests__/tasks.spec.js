4219f4365aa2931ebe27b533fb5aa5deab0d7060
sanfarish <farisfalah@gmail.com>
2024-12-07 21:14:53 +0700

    backend: update jest test

--- backend/__tests__/tasks.spec.js
+++ backend/__tests__/tasks.spec.js
@@ -1,24 +1,43 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const request = require('supertest');
-const app = require('../src/app');
-const models = require('../src/models');
+const request = require("supertest");
+const app = require("../src/app");
+const db = require("../src/models");
 
-jest.mock('morgan', () => jest.fn(() => (req, res, next) => next()));
+jest.mock("morgan", () => jest.fn(() => (req, res, next) => next()));
 
-describe('tasks routes test', () => {
+describe('GET /api/v1/tasks', () => {
+
+    let testDB = db;
+    const dummy = { task: "dummy task", done: false }
+    const mockResult = {
+        ...dummy,
+        id: 1,
+        createdAt: null,
+        updatedAt: null
+    }
     
-    describe('GET /api/v1/tasks', () => {
+    beforeAll(async () => {
+        await testDB.sequelize.sync({ force: true, match: /_test$/ });
+        const res = await testDB.tasks.create(dummy);
+        const createdDate = new Date(res.dataValues.createdAt);
+        const updatedDate = new Date(res.dataValues.updatedAt);
+        createdDate.setMilliseconds(0);
+        updatedDate.setMilliseconds(0);
+        mockResult.createdAt = createdDate.toISOString();
+        mockResult.updatedAt = updatedDate.toISOString();
+    })
+    
+    it('should return status 200 with expected body, when given appropriate request', async () => {
         
-        const mockData = [{ id: 1, task: "a task", done: false, createdAt: "2024-01-01T00:00", updatedAt: "2024-01-01T00:00" }]
-        const mockDB = jest.spyOn(models.tasks, 'findAll');
-        mockDB.mockImplementationOnce(() => Promise.resolve(mockData));
-        
-        it('should return status 200 with expected body, when given appropriate request', async () => {
-            
-            const res = await request(app).get('/api/v1/tasks');
-            expect(res.statusCode).toBe(200);
-            expect(res.body).toStrictEqual(mockData);
-        });
+        const res = await request(app).get('/api/v1/tasks');
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toBeInstanceOf(Array)
+        expect(res.body[0]).toMatchObject(mockResult);
+
     });
+    
+    afterAll(async () => {
+        await testDB.sequelize.close();
+    })
 });
--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,24 +1,25 @@
 module.exports = {
   "development": {
-    "username": process.env.DEV_DB_USERNAME,
-    "password": process.env.DEV_DB_PASSWORD,
+    "username": process.env.DEV_DB_USER,
+    "password": process.env.DEV_DB_PASS,
     "database": process.env.DEV_DB_NAME,
-    "host": process.env.DEV_DB_HOSTNAME,
+    "host": process.env.DEV_DB_HOST,
     "dialect": "mysql",
     "logging": false
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "todo_memo",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.TEST_DB_USER,
+    "password": process.env.TEST_DB_PASS,
+    "database": process.env.TEST_DB_NAME,
+    "host": process.env.TEST_DB_HOST,
+    "dialect": "mysql",
+    "logging": false
   },
   "production": {
-    "username": process.env.PROD_DB_USERNAME,
-    "password": process.env.PROD_DB_PASSWORD,
+    "username": process.env.PROD_DB_USER,
+    "password": process.env.PROD_DB_PASS,
     "database": process.env.PROD_DB_NAME,
-    "host": process.env.PROD_DB_HOSTNAME,
+    "host": process.env.PROD_DB_HOST,
     "dialect": "postgres",
     "logging": false
   }
