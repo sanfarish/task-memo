241497898fdcc549edb6f37f2f02f01394110da6
sanfarish <farisfalah@gmail.com>
2024-12-08 22:59:01 +0700

    backend: update test structure

--- backend/__tests__/tasks.spec.js
+++ backend/__tests__/tasks.spec.js
@@ -6,38 +6,41 @@ const db = require("../src/models");
 
 jest.mock("morgan", () => jest.fn(() => (req, res, next) => next()));
 
-describe('GET /api/v1/tasks', () => {
+describe("tasks route tests", () => {
 
-    let testDB = db;
-    const dummy = { task: "dummy task", done: false }
-    const mockResult = {
-        ...dummy,
-        id: 1,
-        createdAt: null,
-        updatedAt: null
-    }
+    const testDB = db;
     
-    beforeAll(async () => {
+    beforeEach(async () => {
         await testDB.sequelize.sync({ force: true, match: /_test$/ });
-        const res = await testDB.tasks.create(dummy);
-        const createdDate = new Date(res.dataValues.createdAt);
-        const updatedDate = new Date(res.dataValues.updatedAt);
-        createdDate.setMilliseconds(0);
-        updatedDate.setMilliseconds(0);
-        mockResult.createdAt = createdDate.toISOString();
-        mockResult.updatedAt = updatedDate.toISOString();
-    })
-    
-    it('should return status 200 with expected body, when given appropriate request', async () => {
-        
-        const res = await request(app).get('/api/v1/tasks');
-        expect(res.statusCode).toBe(200);
-        expect(res.body).toBeInstanceOf(Array)
-        expect(res.body[0]).toMatchObject(mockResult);
-
     });
     
     afterAll(async () => {
+        await testDB.sequelize.drop();
         await testDB.sequelize.close();
-    })
+    });
+    
+    describe("GET /api/v1/tasks", () => {
+
+        const mockData = { task: "mock task", done: false };
+        const mockResult = {
+            ...mockData,
+            id: 1,
+            createdAt: null,
+            updatedAt: null
+        };
+    
+        beforeEach(async () => {
+            const res = await testDB.tasks.create(mockData);
+            mockResult.createdAt = res.dataValues.createdAt.toISOString();
+            mockResult.updatedAt = res.dataValues.updatedAt.toISOString();
+        });
+        
+        it("should return all tasks", async () => {
+            
+            const res = await request(app).get("/api/v1/tasks");
+            expect(res.statusCode).toBe(200);
+            expect(res.body).toBeInstanceOf(Array);
+            expect(res.body[0]).toMatchObject(mockResult);
+        });
+    });
 });
