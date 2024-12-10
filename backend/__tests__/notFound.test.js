f8eb031b10a98746613b398fbb44312d0407700a
sanfarish <farisfalah@gmail.com>
2024-12-10 14:42:38 +0700

    backend: completed test with more than 80% coverage

--- backend/__tests__/notFound.test.js
+++ backend/__tests__/notFound.test.js
@@ -1,5 +1,6 @@
 const request = require("supertest");
 const app = require("../src/app");
+const endHandler = require("../src/middlewares/endHandler");
 
 jest.mock("morgan", () => jest.fn(() => (req, res, next) => next()));
 
@@ -12,5 +13,16 @@ describe("not found route test", () => {
     it("should return 404 status code", async () => {
         const res = await request(app).get("/");
         expect(res.statusCode).toBe(404);
+    });
+
+    it("should have been called 'next' function when no 'err' value passed", async () => {
+
+        const err = null;
+        const req = jest.fn();
+        const res = { status: jest.fn(() => {return { json: jest.fn() }}) };
+        const next = jest.fn();
+
+        endHandler.error(err, req, res, next);
+        expect(next).toHaveBeenCalled();
     });
 });
